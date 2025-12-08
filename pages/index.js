f5d1af7e38b3fe045f6b6df6c54b028e76579bc8
Gustavo Kenji Kuroda de Oliveira <75386263+GustavoKuroda@users.noreply.github.com>
2025-12-08 11:23:18 +0000

    feat: Add scrips `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home (){
-    return <h1>A excelência é um hábito, não um feito. Concentre-se no progresso constante. ΑΩ</h1>
+function Home() {
+  return (
+    <h1>
+      A excelência é um hábito, não um feito. Concentre-se no progresso
+      constante. ΑΩ
+    </h1>
+  );
 }
 
 export default Home;
